2b79aad518061d7af39e018689142591936dc466
coder0112 <zafarbekegamov0112@gmail.com>
2022-11-11 12:20:07 +0500

    pass all tests

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double tax = 0;
+        if (salary <= 0) {
+            System.out.println("wrong input!");
+        } else if (salary <= 10000) {
+            System.out.println(salary * .85);
+        } else if (salary > 10000 && salary <= 20000) {
+            System.out.println(salary * .82);
+        } else {
+            System.out.println(salary * .8);
+        }
     }
 }
